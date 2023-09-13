b809a4b114c0f642d89cce68475c2869a0c4ad6c
ARAVINDHAN PARTHA <143607886+jdaravindhan7@users.noreply.github.com>
2023-09-13 21:09:48 +0530

    Constructor

---
+++ Exp12-Constructor.java
@@ -0,0 +1,22 @@
+import java.io.*;
+class ceat
+{
+	int b,h;
+	ceat()
+	{
+		b=7;
+		h=6;
+	}
+	void dream()
+	{
+		float a;
+		a=b*h;
+		System.out.println(a);
+	}
+}		
+public class Main {
+	public static void main(String[] args) {
+		ceat k=new ceat();
+		k.dream();
+	}
+}
