5cb7442f1ff207a74702718da9d54a6afdb7039b
ARAVINDHAN PARTHA <143607886+jdaravindhan7@users.noreply.github.com>
2023-09-25 23:40:55 +0530

    StringOperations

---
+++ Exp28-StringOperations.java
@@ -0,0 +1,9 @@
+import java.io.*;
+public class Main {
+	public static void main(String[] args)throws Exception {
+		String h="arav";
+		String b="kumar";
+		System.out.println(h.concat(b));
+		
+	}
+}
