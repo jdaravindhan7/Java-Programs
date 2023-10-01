3fe1aacfc22b2307d8c58dae9404043ceca4e549
ARAVINDHAN PARTHA <143607886+jdaravindhan7@users.noreply.github.com>
2023-10-01 21:53:06 +0530

    CharacterValues

---
+++ Exp30-CharacterValues.java
@@ -0,0 +1,10 @@
+import java.io.*;
+public class Main {
+	public static void main(String[] args)throws Exception {
+		char k;
+		DataInputStream d=new DataInputStream(System.in);
+		k='g';
+		int y=k;
+		System.out.println("ascii value of "+k+" is "+y);
+	}
+}
